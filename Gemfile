da41b192c50b7f1290637c3acd31fb316b8252b9
David Bayendor <dbayendor@gmail.com>
2013-08-27 22:05:00 -0600

    Initial Commit

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
 source 'https://rubygems.org'
 
+gem 'rspec'
 gem 'guard-rspec'
-gem 'rb-fsevent'
-gem 'growl'
+gem 'debugger'
+gem 'rb-fsevent', '~> 0.9.1'
+
+group :test, :darwin, :development do
+  gem 'ruby_gntp'
+end
---
+++ lib/calculator.rb
@@ -0,0 +1,28 @@
+class Calculator
+  attr_accessor :total
+
+  def initialize
+    self.total = 0
+  end
+
+  def add(number)
+    self.total += number.to_f
+    self
+  end
+
+  def subtract(number)
+    self.total -= number.to_f
+    self
+  end
+
+  def multiply(number)
+    self.total *= number.to_f
+    self
+  end
+
+  def divide(number)
+    self.total /= number.to_f
+    self
+  end
+
+end
